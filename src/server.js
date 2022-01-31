ac8d884db2e9a6d838b5c8825eb4c0520285d90d
Diego Anderson <dieanderson@gmail.com>
2022-01-31 18:24:35 -0300

    estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+//const path = require('path')
+
+//const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+//Connect database
+//db.connect()
+
+//Settings
+
+//Enable server receive data post method(form)
+app.use(express.urlencoded({ extended: true }))
+
+//Def routes
+app.use('/api', routes)
+
+//Run the Server
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
